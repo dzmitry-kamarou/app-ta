80ef293211be96584fd44b26af40ad29c3879532
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-03-25 22:23:27 +0300

    ATA-3: account bo added

---
+++ src/main/java/com/dzmitrykamarou/app/ta/business/account/Account.java
@@ -0,0 +1,14 @@
+package com.dzmitrykamarou.app.ta.business.account;
+
+public class Account {
+
+  private Long id;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
