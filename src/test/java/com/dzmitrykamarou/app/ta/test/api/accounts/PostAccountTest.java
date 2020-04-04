eda13819ba6e68250b17a7050e74f5b28a1b5c9e
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-04-04 10:48:00 +0300

    ATA-3: add after class in POST /accounts test

--- src/test/java/com/dzmitrykamarou/app/ta/test/api/accounts/PostAccountTest.java
+++ src/test/java/com/dzmitrykamarou/app/ta/test/api/accounts/PostAccountTest.java
@@ -9,6 +9,7 @@ import com.dzmitrykamarou.app.ta.business.account.AccountFactory;
 import com.dzmitrykamarou.app.ta.config.AppConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,10 +23,11 @@ public class PostAccountTest {
     response = given()
         .baseUri(AppConfig.config.host())
         .port(AppConfig.config.port())
+        .basePath("/accounts")
         .contentType(ContentType.JSON)
         .body(account = AccountFactory.randomAccount())
         .when()
-        .post("/accounts");
+        .post();
   }
 
   @Test(description = "Response code test")
@@ -40,5 +42,19 @@ public class PostAccountTest {
         .body("firstName", equalTo(account.getFirstName()))
         .body("lastName", equalTo(account.getLastName()))
         .body("id", greaterThan(0));
+  }
+
+  @AfterClass
+  public void tearDown() {
+    given()
+        .baseUri(AppConfig.config.host())
+        .port(AppConfig.config.port())
+        .basePath("/accounts/")
+        .pathParam("id", response
+            .body()
+            .jsonPath()
+            .get("id"))
+        .when()
+        .delete("/{id}");
   }
 }
