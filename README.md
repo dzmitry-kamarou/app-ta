de9c463df4f682b3a5ddadc6777e726b57498fe7
dzmitry-kamarou <dmitry.komarov@oxagile.com>
2020-04-19 13:46:37 +0300

    Ata 3 (#2) (#3)
    * ATA-3: pom.xml added
    
    * ATA-3: account bo added
    
    * ATA-3: add POST /accounts code, body tests
    
    * ATA-3: add after class in POST /accounts test
    
    * ATA-3: add GetAccountTest.java
    
    * ATA-3: add delete account test
    
    * ATA-3: add qa env url
    
    * ATA-3: rename app-ta to diamond-taf-java
    
    * ATA-3: change canban pipeline

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# app-ta
-Test Automation framework for app application
+# diamond-taf-java
+Test automation framework written in Java for [diamond](https://github.com/dzmitry-kamarou/diamond) application
---
+++ src/main/java/com/dzmitrykamarou/diamond/taf/business/account/Account.java
@@ -0,0 +1,55 @@
+package com.dzmitrykamarou.diamond.taf.business.account;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Calendar;
+
+public class Account {
+
+  private Long id;
+  private String firstName;
+  private String lastName;
+  @JsonIgnore
+  private Calendar createdAt;
+  @JsonIgnore
+  private Calendar updatedAt;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Calendar getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Calendar createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Calendar getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Calendar updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
