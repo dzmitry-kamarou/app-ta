0ad1a8be71c05e6abc5b6d8a7dd377d16b5f25b3
dzmitry-kamarou <kosarev.gomel@gmail.com>
2020-06-21 16:12:24 +0300

    DIAM-1: refactor healthcheck test

---
+++ src/test/java/com/dzmitrykamarou/diamond/taf/test/api/healthcheck/HealthcheckCanBeReceivedTest.java
@@ -0,0 +1,23 @@
+package com.dzmitrykamarou.diamond.taf.test.api.healthcheck;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.dzmitrykamarou.diamond.taf.api.flow.HealthcheckFlow;
+import org.testng.annotations.Test;
+
+@Test(suiteName = "Healthcheck suite", groups = {"smoke", "regression"})
+public class HealthcheckCanBeReceivedTest {
+
+  String upStatus = "UP";
+
+  @Test(description = "BD status test")
+  public void dbStatusTest() {
+    assertThat("DB status should be UP", HealthcheckFlow.dbStatus(), equalTo(upStatus));
+  }
+
+  @Test(description = "API status test")
+  public void apiStatusTest() {
+    assertThat("API status should be UP", HealthcheckFlow.apiStatus(), equalTo(upStatus));
+  }
+}
